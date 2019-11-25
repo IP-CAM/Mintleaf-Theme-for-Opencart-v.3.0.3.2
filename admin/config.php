fe7ff64df1118896b4886088d9195b113093fb85
Long Ricker <philongvadung@gmail.com>
2019-11-25 10:03:58 +0700

    update

--- admin/config.php
+++ admin/config.php
@@ -25,7 +25,7 @@ define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', '192.168.1.125');
+define('DB_HOSTNAME', '192.168.43.43');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', 'root');
 define('DB_DATABASE', 'opencart');
